75853ed8a68caed789929f9936e939d3dbd1f7e5
Gitsantos <santosh984970@gmail.com>
2017-11-03 08:48:24 +0900

    Initial commit

---
+++ app/src/main/java/com/example/nepblog/MyApp.java
@@ -0,0 +1,16 @@
+package com.example.nepblog;
+
+import android.app.Application;
+
+
+/**
+ * Created by santoshrai on 10/31/17.
+ */
+
+public class MyApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+}
+}
